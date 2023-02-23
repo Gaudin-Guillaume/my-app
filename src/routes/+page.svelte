513c5e31d009cfec360b88b809ac620eb3f0ae7c
Aristide <aristide.bruneau@gmail.com>
2023-02-23 17:34:10 +0100

    h1 stylé ;)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Lead the change.</h1>
-
+<h1>
+	<span
+		class="bg-gradient-to-br from-primary-500 via-tertiary-500 to-secondary-500 bg-clip-text text-transparent box-decoration-clone"
+	>
+		Lead the change
+	</span>
+</h1>
