ef3eccefee75c49bf8342ddf63e01f9a0f3b2ef7
Aristide <aristide.bruneau@gmail.com>
2023-02-23 16:21:51 +0100

    Fix button load > move to lib folder + rename

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,47 +1,37 @@
-
 <script>
-    
-    import Buton from '$comp/Buton.svelte'
+	import Button from './components/Button.svelte';
 </script>
 
 <nav>
-    <ul>
-        <li><a href="./">Home</a></li>
-        <li><a href='about'>about</a></li>
-        <li><a href="#">Biocontrol</a></li>
-        <li><a href="#">contact</a></li>
-     </ul>
+	<ul>
+		<li><a href="./">Home</a></li>
+		<li><a href="about">about</a></li>
+		<li><a href="#">Biocontrol</a></li>
+		<li><a href="#">contact</a></li>
+	</ul>
 
-     <Buton />
-
-     
-
-     
-    
+	<Button />
 </nav>
 
 <style>
-    nav{
-        background-color:rgb(207, 207, 207);
-        width:100vw;
-        height:100px;
-        color:#404040;
-        font-weight:600;
-        position:sticky;
-        top:0;
-        font-family:sans-serif;
-        margin:0 !important;
-        opacity:0.2;
-       
-       
-    }
+	nav {
+		background-color: rgb(207, 207, 207);
+		width: 100vw;
+		height: 100px;
+		color: #404040;
+		font-weight: 600;
+		position: sticky;
+		top: 0;
+		font-family: sans-serif;
+		margin: 0 !important;
+		opacity: 0.2;
+	}
 
-    ul{
-        list-style-type:none;
-    }
+	ul {
+		list-style-type: none;
+	}
 
-    nav ul li{
-        display:inline;
-    }
+	nav ul li {
+		display: inline;
+	}
 </style>
-
---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,12 @@
+<script>
+	let content = 'Default value';
+</script>
+
+<button>{content}</button>
+
+<style>
+	button {
+		color: white;
+		background-color: #15514f;
+	}
+</style>
