38f5531817a76c541add92c96cd15c255ccac347
Gaudin-Guillaume <112545222+Gaudin-Guillaume@users.noreply.github.com>
2023-07-04 15:27:40 +0200

    test config vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ const config = {
 	plugins: [sveltekit()]
 };
 
-export default config;
+export default defineConfig({
+	esbuild: {
+	  supported: {
+		'top-level-await': true //browsers can handle top-level-await features
+	  },
+	}
+  });
