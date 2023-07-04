e2ec0a5c8528973fe83b99c464f4f4b94d2572fb
Gaudin-Guillaume <112545222+Gaudin-Guillaume@users.noreply.github.com>
2023-07-04 14:26:47 +0200

    test

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,12 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		env:{
-			dir:'./'
-		},
 		adapter: adapter()
+		vite: {
+			define: {
+			  'process.env': process.env,
+			},},
+			target: '#svelte',
 	},
 
 	preprocess: [
